35179e6c16277dcfbbc7b832d4d6914cb680b3be
Alice <alice_starbiju@outlook.com>
2025-11-11 10:02:15 -0300

    Refactor Jantar class initialization method

--- Jantar.java
+++ Jantar.java
@@ -3,16 +3,16 @@ public class Jantar {
     private Garfo[] garfos;
 
     public void iniciar() {
-        // Inicializa o ambiente com 5 filósofos e 5 garfos
+        
         garfos = new Garfo[5];
         filosofos = new Fisolofo[5];
 
-        // Cria 5 garfos
+       
         for (int i = 0; i < 5; i++) {
             garfos[i] = new Garfo();
         }
 
-        // Cria 5 filósofos, cada um com dois garfos
+      
         for (int i = 0; i < 5; i++) {
             Garfo garfoEsquerdo = garfos[i];
             Garfo garfoDireito = garfos[(i + 1) % 5];
@@ -28,3 +28,4 @@ public class Jantar {
         return garfos;
     }
 }
+
