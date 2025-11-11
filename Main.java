4fdfa7765c509ddd45e828a212bb70a1b9472d80
Alice <alice_starbiju@outlook.com>
2025-11-11 10:13:08 -0300

    Update and rename JantarTest.java to Main.java

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int tempoPensar = 1000; 
+        int tempoComer = 1500;  
+
+        Jantar jantar = new Jantar(tempoPensar, tempoComer);
+        jantar.iniciarJantar();
+    }
+}
