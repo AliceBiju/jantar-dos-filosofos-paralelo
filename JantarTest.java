681d72d889f2a201587c35a2b51e44ee01b626cb
Alice <alice_starbiju@outlook.com>
2025-11-11 10:29:21 -0300

    Update JantarTest.java

--- JantarTest.java
+++ JantarTest.java
@@ -9,36 +9,46 @@ class JantarTest {
 
     @BeforeEach
     void setUp() {
-  
         jantar = new Jantar(1000, 1500);
         jantar.iniciarJantar();
     }
 
     @Test
     void testFilosofosExistem() {
+        Fisolofo[] filosofos = jantar.getFilosofos();
+        assertEquals(5, filosofos.length, "Deve haver 5 filósofos");
 
-        assertEquals(5, jantar.getFilosofos().length, "Deve haver 5 filósofos");
-
-
-        for (Fisolofo f : jantar.getFilosofos()) {
-            assertNotNull(f, "O filósofo não deve ser nulo");
+        for (Fisolofo f : filosofos) {
+            assertNotNull(f, "Filósofo não deve ser nulo");
+            assertNotNull(f.getGarfoEsquerdo(), "Filósofo deve ter garfo esquerdo");
+            assertNotNull(f.getGarfoDireito(), "Filósofo deve ter garfo direito");
         }
     }
 
     @Test
     void testGarfosExistem() {
-        assertEquals(5, jantar.getGarfos().length, "Deve haver 5 garfos");
+        Garfo[] garfos = jantar.getGarfos();
+        assertEquals(5, garfos.length, "Deve haver 5 garfos");
 
-        for (int i = 0; i < jantar.getGarfos().length; i++) {
-            for (int j = i + 1; j < jantar.getGarfos().length; j++) {
-                assertNotSame(jantar.getGarfos()[i], jantar.getGarfos()[j],
-                        "Garfos não devem ser o mesmo objeto");
+        for (int i = 0; i < garfos.length; i++) {
+            for (int j = i + 1; j < garfos.length; j++) {
+                assertNotSame(garfos[i], garfos[j], "Garfos não devem ser o mesmo objeto");
             }
         }
     }
 
     @Test
-    void testFalhaProposital() {
-        fail("Teste propositalmente falhando para cumprir requisito da tarefa");
+    void testFilosofosPegamGarfosCorretamente() {
+        Fisolofo[] filosofos = jantar.getFilosofos();
+        for (int i = 0; i < filosofos.length; i++) {
+            Fisolofo f = filosofos[i];
+            Garfo esquerdo = f.getGarfoEsquerdo();
+            Garfo direito = f.getGarfoDireito();
+
+
+            int idDireitoEsperado = (esquerdo.getId() + 1) % 5;
+            assertEquals(idDireitoEsperado, direito.getId(),
+                    "O garfo direito deve ser o próximo na mesa em relação ao garfo esquerdo");
+        }
     }
 }
