21f858bfe249d8c28e221d6a1c08d27a44a57bdf
Alice <alice_starbiju@outlook.com>
2025-11-11 10:11:56 -0300

    Refactor Jantar class with constructor and initialization
    Refactor Jantar class to include constructor and initialize philosophers and forks with parameters.

--- Jantar.java
+++ Jantar.java
@@ -1,22 +1,28 @@
 public class Jantar {
-    private Fisolofo[] filosofos;
-    private Garfo[] garfos;
+    private final Fisolofo[] filosofos;
+    private final Garfo[] garfos;
+    private final int NUM_FILOSOFOS = 5;
 
-    public void iniciar() {
-        
-        garfos = new Garfo[5];
-        filosofos = new Fisolofo[5];
+    public Jantar(int tempoPensar, int tempoComer) {
+        garfos = new Garfo[NUM_FILOSOFOS];
+        filosofos = new Fisolofo[NUM_FILOSOFOS];
 
-       
-        for (int i = 0; i < 5; i++) {
-            garfos[i] = new Garfo();
+
+        for (int i = 0; i < NUM_FILOSOFOS; i++) {
+            garfos[i] = new Garfo(i);
         }
 
-      
-        for (int i = 0; i < 5; i++) {
+    
+        for (int i = 0; i < NUM_FILOSOFOS; i++) {
             Garfo garfoEsquerdo = garfos[i];
-            Garfo garfoDireito = garfos[(i + 1) % 5];
-            filosofos[i] = new Fisolofo(garfoEsquerdo, garfoDireito);
+            Garfo garfoDireito = garfos[(i + 1) % NUM_FILOSOFOS];
+            filosofos[i] = new Fisolofo(i, garfoEsquerdo, garfoDireito, tempoPensar, tempoComer);
+        }
+    }
+
+    public void iniciarJantar() {
+        for (Fisolofo f : filosofos) {
+            f.start();
         }
     }
 
